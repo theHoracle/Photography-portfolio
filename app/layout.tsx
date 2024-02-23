c00360c6dce71bb701620d2e68756a0dfd5597cb
theHoracle <princemmjames2@gmail.com>
2024-02-23 16:57:45 +0100

    added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const raleway = Raleway({ subsets: ["latin"] });
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={raleway.className}>{children}</body>
+      <body className={raleway.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
